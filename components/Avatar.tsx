984169bd40b803c4aaba7854dbf36b95152e36f1
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-05-19 21:28:22 -0300

    PostBox

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -2,13 +2,18 @@ import { useSession } from 'next-auth/react'
 import Image from 'next/image';
 import React from 'react'
 
-const Avatar = () => {
+type Props = {
+    seed?:string
+    large?: boolean
+}
+
+const Avatar = ({seed, large}: Props) => {
 
     const {data: session} = useSession();
 
     return (
-        <div className="relative h-10 w-10 rounded-full border-gray-300 bg-white">
-            <Image layout="fill" src={`https://avatars.dicebear.com/api/open-peeps/${session?.user?.name || 'placeholder'} `} />
+        <div className={`relative h-10 w-10 rounded-full border-gray-300 bg-white ${large && 'h-20 w-20'}`}>
+            <Image layout="fill" src={`https://avatars.dicebear.com/api/open-peeps/${seed || session?.user?.name || 'placeholder'}.svg`} />
         </div>
     )
 }
--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,16 +1,41 @@
 import { useSession } from 'next-auth/react'
 import React from 'react'
 import Avatar from "./Avatar"
+import {LinkIcon, PhotographIcon} from "@heroicons/react/outline"
+import {useForm} from "react-hook-form"
+
+type FormData= {
+    postTitle: string,
+    postBody: string,
+    postImage: string,
+    subreddit: string,
+}
 
 const PostBox = () => {
     const {data:session} = useSession();
+    const {
+        register,
+        setValue,
+        handleSubmit,
+        watch,
+        formState: {errors},
+    } = useForm<FormData>()
+
 
     return (
-        <form>
+        <form className="sticky top-16 z-50 rounded-md border border-gray-300 bg-white p-2">
             <div className="flex items-center space-x-3">
                 <Avatar/>
-                <input disabled={!session} className="rounded-md flex-1 bg-gray-50 p-2 pl-5 outline-none" type="text" placeholder={session? "Create a post by entering a title!" : "Sign in to post!" }/>
+                <input {...register("postTitle", {required:true})} disabled={!session} className="rounded-md flex-1 bg-gray-50 p-2 pl-5 outline-none" type="text" placeholder={session? "Create a post by entering a title!" : "Sign in to post!" }/>
+                <PhotographIcon className={`h-6 text-gray-400 cursor-pointer`}/>
+                <LinkIcon className="h-6 text-gray-400 cursor-pointer"/>
             </div>
+
+            {!!watch("postTitle")&&(
+                <div>
+                    <h1>BOOM</h1>
+                </div>
+            )}
         </form>
     )
 }
