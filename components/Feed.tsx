1876633988b2e0392775951fe22af6b263a6322a
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-05-23 22:57:12 -0300

    BugFixed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,6 +3,8 @@ import { GET_ALL_POSTS, GET_ALL_POSTS_BY_TOPIC } from '../graphql/queries'
 import {useQuery} from "@apollo/client"
 import Post from "./Post"
 import {Jelly} from "@uiball/loaders"
+import { GET_SUBREDDITS_WITH_LIMIT } from '../graphql/queries'
+import SubredditRow from "../components/SubredditRow"
 
 type Props = {
   topic?: string,
@@ -19,15 +21,35 @@ const Feed = ({topic}: Props) => {
 
   console.log(error)
 
-  
+  const {data: subredditData} = useQuery(GET_SUBREDDITS_WITH_LIMIT, {
+    variables: {
+      limit: 10,
+    }
+  })
+
+  const subreddits: Subreddit[] = subredditData?.getSubredditListLimit;
+  console.log(subreddits)
 
   return (
     <div className="mt-5 space-y-4">
       {
         posts? 
-          posts?.map((post) => (
-            <Post key={post.id} post={post}/>
-          ))
+          <div className="flex-1 flex">
+            <div>
+              {
+                posts?.map((post) => (
+                  <Post key={post.id} post={post}/>
+                ))
+              }
+            </div>  
+            
+            <div className="sticky top-36 mx-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline">
+              <p className="text-md mb-1 p-4 pb-3 font-bold">Top Communities</p>
+              {subreddits?.map((subreddit, i) => (
+                <SubredditRow topic={subreddit.topic} index={i} key={subreddit.id}/>
+              ))}
+            </div>
+          </div>
         :
         <div className="flex w-full items-center justify-center p-10 text-xl">
           <Jelly size={50} color="#ff4501"/>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import PostBox from "../components/PostBox"
 import Feed from "../components/Feed"
-import { GET_SUBREDDITS_WITH_LIMIT } from '../graphql/queries'
-import {useQuery} from "@apollo/client"
-import SubredditRow from "../components/SubredditRow"
 
 const Home: NextPage = () => {
 
-  const {data} = useQuery(GET_SUBREDDITS_WITH_LIMIT, {
-    variables: {
-      limit: 10,
-    }
-  })
 
-  const subreddits: Subreddit[] = data?.getSubredditListLimit;
-  console.log("Subreddits:", subreddits)
   return (
     <div className="max-w-5xl my-7 mx-auto">
       <Head>
@@ -24,19 +14,9 @@ const Home: NextPage = () => {
 
       <PostBox/>
 
-      <div className="flex-1 flex">
+      <div className="flex-1">
         <Feed />
-        <div className="sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline">
-          <p className="text-md mb-1 p-4 pb-3 font-bold">Top Communities</p>
-          <div>
-            {/* List subreddit */}
-            <div>
-              {subreddits?.map((subreddit, i) => (
-                <SubredditRow topic={subreddit.topic} index={i} key={subreddit.id}/>
-              ))}
-            </div>
-          </div>
-        </div>
+
       </div>
     </div>
   )
